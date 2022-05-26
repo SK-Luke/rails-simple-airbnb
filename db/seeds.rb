063b10151c4ebe7ceb431c7764c6a0a5eaada01d
sekkun foo <sekkun.luke@gmail.com>
2022-05-26 16:12:01 +0800

    styled displayed all flats

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating flats..."
+4.times do
+  flat = Flat.new({
+    name: Faker::Fantasy::Tolkien.location,
+    address: Faker::Address.street_address,
+    description: Faker::Fantasy::Tolkien.poem,
+    price_per_night: rand(50..300),
+    number_of_guests: rand(1..5)
+  })
+  flat.save
+end
+puts "Finished!"
